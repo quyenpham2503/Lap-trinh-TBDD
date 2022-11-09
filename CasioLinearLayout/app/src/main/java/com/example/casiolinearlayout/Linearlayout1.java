93e769c8266056e3262bb14ecba47c1d5868e180
quyenpham2503 <73056961+quyenpham2503@users.noreply.github.com>
2022-11-09 22:59:17 +0700

    Linear layout

---
+++ CasioLinearLayout/app/src/main/java/com/example/casiolinearlayout/Linearlayout1.java
@@ -0,0 +1,14 @@
+package com.example.casiolinearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Linearlayout1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linearlayout1);
+    }
+}
