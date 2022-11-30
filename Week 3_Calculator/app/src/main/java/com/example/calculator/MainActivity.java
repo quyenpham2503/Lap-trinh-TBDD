0ebc096e79248feff6d1367466496e13c97ee4ff
quyenpham2503 <73056961+quyenpham2503@users.noreply.github.com>
2022-11-30 19:26:33 +0700

    Add files via upload
    Bài tập tuần 3

---
+++ Week 3_Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button butCalculator;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        butCalculator = (Button) findViewById(R.id.butCalculator);
+        butCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                openCalculatorPage();
+            }
+        });
+
+    }
+
+    public void openCalculatorPage() {
+        Intent intent = new Intent(this, Calculator_Activity.class);
+        startActivity(intent);
+    }
+
+}
